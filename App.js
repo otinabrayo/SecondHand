f177efbdc9c4c5186e3e1edd4f7355e8142f4f0e
brian <brianotina20@gmail.com>
2024-09-29 22:03:38 +0300

    Native start

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  console.log("App executed");
+  let x;
+  x.toString();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up Maaam</Text>
       <StatusBar style="auto" />
     </View>
   );
